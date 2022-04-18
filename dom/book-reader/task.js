c04abc574906b6f676f38bfefc002ad700bdbbba
Ilbakhtin Sergey <sonyk753@gmail.com>
2022-04-19 00:01:26 +0500

    исправления book-reader

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -3,10 +3,13 @@ const size = Array.from(document.querySelectorAll('.font-size'));
 const active = document.querySelector('.font-size_active');
 
 
+
 for(i in size) {
-    console.log(size[i].classList.contains('font-size_active'))
     
     size[0].addEventListener('click', (event) => {
+        for(k in size) {
+            size[k].classList.remove("font-size_active");
+        }
         book.classList.add('book_fs-small');
         book.classList.remove('book_fs-big');
         size[0].classList.add('font-size_active');
@@ -14,9 +17,13 @@ for(i in size) {
     })
     
     size[2].addEventListener('click', (event) => {
+        for(k in size) {
+            size[k].classList.remove("font-size_active");
+        }
         book.classList.add('book_fs-big');
         book.classList.remove('book_fs-small');
         size[2].classList.add('font-size_active');
         event.preventDefault();
     })
+    
 }
